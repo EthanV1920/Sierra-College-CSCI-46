a28adb12d018e7f943ee49e6d461fc80abc2701d
Ethan Vosburg <ethan@vosburg.us>
2022-05-09 13:51:18 -0700

    Level 5 Final Submitted Code
    Finally got project working. Made some minor changes and re positioned jpeg saving script.

--- Project 5/Level 5/level_5.c
+++ Project 5/Level 5/level_5.c
@@ -12,10 +12,11 @@
 #include <unistd.h>
 #include <stdint.h>
 
+// Perform an bitwise operation to get int from uint8_t unsigned char
 #define bytes_to_u8(MSB,LSB) (((unsigned int) ((unsigned char) MSB)) & 255)<<8 | (((unsigned char) LSB)&255) 
 
 
-// Function prototypes. Don't change these.
+// Function prototypes
 uint8_t *read_card(char *fname, int *size);
 void save_jpeg(uint8_t *data, int size, char *filename);
 void recover(uint8_t *data, int size);
@@ -119,22 +120,23 @@ void recover(uint8_t *data, int size){
     char tempName[15];
     
 
+    // Itterate through blocks of data looking for start sequence
     for(int i = 0; i < size; i+=512){
         reassign(data_check_ptr, data, i, 4);
         block_count++;
 
 
-       
+        // Identify start marker and appliucation specific marker 
         if(compare_array(data_check_ptr, SOIaPtr, 2) || compare_array(data_check_ptr, SOIbPtr, 2)){
             reassign(data_check_ptr, data, i+2, 4);
             if(compare_array(data_check_ptr, E0Ptr, 2) || compare_array(data_check_ptr, E1Ptr, 2)){
                 printf("size: %d i: %d\n", size, i/16 +1);
                 printf("    NOTE: found E0 or E1        %d\n",skip(data,i) );
-                JPEGstart = i;
-                i = i + skip(data,i+2);
+                JPEGstart = i;  //Define Jpeg Start byte
                 img_count++;
             
-                for(int j = i; j<size; j++){ 
+                // Iterate through Jpeg data and identify markers and skip
+                for(int j = i + skip(data,i+2); j<size; j++){ 
                     reassign(data_check_ptr, data, j, 2);
                     if(compare_array(data_check_ptr, SOF0Ptr, 2)){
                         printf("    NOTE: found SOF0\n");
@@ -152,6 +154,10 @@ void recover(uint8_t *data, int size){
                         printf("    NOTE: found DQT\n");
                         j = j + skip(data,j);
                     }
+                    if(compare_array(data_check_ptr, DRIPtr, 2)){
+                        printf("    NOTE: found DRI\n");
+                        j = j + skip(data,j);
+                    }
                     if(compare_array(data_check_ptr, SOSPtr, 2)){
                         printf("    NOTE: found SOS\n");
                         j = j + skip(data,j);
@@ -161,28 +167,23 @@ void recover(uint8_t *data, int size){
                         j = j + skip(data,j);
                     }
                     if(compare_array(data_check_ptr, EOIPtr, 2)){
-                        JPEGfinish = j;
+                        JPEGfinish = j;     // Define Jpeg finish byte
                         printf("    NOTE: found EOI\n");
-                        i=i + ((JPEGfinish-JPEGstart)-(JPEGfinish-JPEGstart)%512);
+                        i=i + ((JPEGfinish-JPEGstart)-(JPEGfinish-JPEGstart)%512);      // Reassign i to block count
                         printf("size: %d i: %d\n", size, i );
                         break;
                     }
                     
-                    
-                    
-                    
-                    
-                //     // printf("    INDEX: %d\n", i);
-
-
-                    uint8_t *tempBuf = malloc(JPEGfinish-JPEGstart);
-                    for(int k = 0; k < (JPEGfinish-JPEGstart); k++){
-                        tempBuf[k] = data[JPEGstart+k];
-                    }
-                    sprintf(tempName, "image %d.jpeg", img_count);
-                    save_jpeg(tempBuf, JPEGfinish-JPEGstart, tempName);
-                    free(tempBuf);
                 }
+
+                uint8_t *tempBuf = malloc(JPEGfinish-JPEGstart);    // Defince buffer with size of jpeg
+                for(int k = 0; k < (JPEGfinish-JPEGstart); k++){
+                    tempBuf[k] = data[JPEGstart+k];
+                }
+                sprintf(tempName, "image %d.jpeg", img_count);
+                save_jpeg(tempBuf, JPEGfinish-JPEGstart, tempName);
+                free(tempBuf);
+
                 printf("NOTE:  found %d in %d blocks\n", img_count, block_count);
             }
             
@@ -195,25 +196,14 @@ void recover(uint8_t *data, int size){
 
 }
 
+// Return number of byte to skip as int when passed data array pointer and current index
 int skip(uint8_t *data, int i){
-    // char temp[1];
-    // uint8_t temp[sizeof(uint8_t)*2];
-    // memcpy(&temp, data[j],1);
-    // memcpy(&temp, data[j+1],1);
-        
-    // for(int k = -4; k<12; k++){
-    //     printf("    %d : %X\n", j+k, (char)data[j+k]);
-    // }
-    // snprintf(temp, 1, "%X%X", (char)data[j+2], (char)data[j+3]);
-
-    // int rec = (int)(((unsigned)data[i+2] << 8) | data[i+3] );
     int rec = bytes_to_u8(data[i+2], data[i+3]);
     printf("    %X -- %d\n", rec, rec);
-    // sprintf(temp, "%X%X", (char)data[j], (char)data[j+1]);
-    // unsigned char buf = data[i+j+1] + data[i+j+2];
     return rec;
 }
 
+// Compare two arrays and return wheather n number of entries starting from the beginning are the same
 int compare_array(int *arr1, int *arr2, int arrSize){
     int isSame = 1;
 
@@ -226,14 +216,9 @@ int compare_array(int *arr1, int *arr2, int arrSize){
 
 }
 
+// assign n items from one array to another array starting at passed index
 void reassign(int *cpArr, uint8_t *srcArr, int index, int amount){
     for(int i = 0; i< amount; i++){
         cpArr[i] = srcArr[index+i];
     }            
-}
-
-void printArray(int interger_array[]){
-  for (int j = 0; j < 4; j++ ) {
-      printf("Element[%d] = %d\n", j, interger_array[j] );
-   }
 }
