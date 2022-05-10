7a70da56df090ee5e985ac79d326967af5973889
Ethan Vosburg <ethan@vosburg.us>
2022-05-10 00:03:52 -0700

    Warm Up 7.1 Initial Commit

---
+++ Project 7/warm_up7-1.c
@@ -0,0 +1,18 @@
+// Ethan Vosburg
+// May 5, 2022
+
+// The user input is compared to the longer string and then takes action as follows:
+
+//   - If the input is contained within the longer string, ignore it. Use strstr to perform the search.
+//   - If the user input is less than the longer string (as indicated by the return value from strcmp), 
+//     prepend the input onto the longer string with a space separating them.
+//   - If the input is greater than the longer string, append it with a space separating them.
+
+
+#include <stdio.h>
+
+
+
+int main(){
+
+}
